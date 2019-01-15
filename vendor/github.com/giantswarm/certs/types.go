b35ffdcca6ed00bcd549fd079f0d4bb97ee8e59a
Paweł Kopiczko <kopiczko@gmail.com>
2019-01-15 16:16:31 +0000

    Update to kubernetes 1.13.2

---
+++ vendor/github.com/giantswarm/certs/types.go
@@ -0,0 +1,26 @@
+package certs
+
+type TLS struct {
+	CA, Crt, Key []byte
+}
+
+type Cluster struct {
+	APIServer        TLS
+	CalicoEtcdClient TLS
+	EtcdServer       TLS
+	ServiceAccount   TLS
+	Worker           TLS
+}
+
+type ClusterOperator struct {
+	APIServer TLS
+}
+
+type Draining struct {
+	NodeOperator TLS
+}
+
+type Monitoring struct {
+	KubeStateMetrics TLS
+	Prometheus       TLS
+}
